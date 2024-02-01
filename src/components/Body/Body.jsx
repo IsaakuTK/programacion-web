d06e7b2df79aab0fb582a5f5bab79313a4ced54e
IsaakuTK <isaaccallecardozo@gmail.com>
2024-02-01 18:59:45 -0500

    Now Have Components

---
+++ src/components/Body/Body.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function Body() {
+    return(
+        <>
+         <body>
+            <h1>Los Juegos Del Calambre</h1>
+        </body>
+         
+        </>
+    )
+}
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function Footer() {
+    return(
+        <>
+         <footer>
+            <h3>Continuara</h3>
+         </footer>
+         
+        </>
+    )
+}
